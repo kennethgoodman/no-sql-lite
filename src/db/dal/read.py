425b6404a0d6cb3930c418d7d7de5b2fbaeaa7c8
kennethgoodman <kennethsgoodman@yahoo.com>
2020-05-07 22:28:17 -0400

    fixing that it looks at the key instead of ts

--- src/db/dal/read.py
+++ src/db/dal/read.py
@@ -8,6 +8,6 @@ def read(key, path):
     with open(path, 'r') as f:
         current_value = None
         for line in f:
-            if line.split(",")[0] == key:
-                current_value = json.loads(",".join(line.split(",")[1:]))
+            if line.split(",")[1] == key:
+                current_value = json.loads(",".join(line.split(",")[2:]))
         return current_value
