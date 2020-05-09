05ce91260981dbfa21cad3e6e175bc2359486667
kennethgoodman <kennethsgoodman@yahoo.com>
2020-05-09 00:54:10 -0400

    dont json parse before needing it

--- src/db/row/row_value.py
+++ src/db/row/row_value.py
@@ -2,15 +2,21 @@ import json
 
 
 class RowValue:
-    def __init__(self, value):
+    def __init__(self, value, raw_value=None):
         self.value = value
+        self.raw_value = raw_value
 
     @staticmethod
     def from_str(value_str):
-        return RowValue(json.loads(value_str))
+        return RowValue(None, value_str)
 
     def to_file_format(self):
         return json.dumps(self.value)
 
     def to_json(self):
+        return self.get_value()
+
+    def get_value(self):
+        if self.value is None:
+            self.value = json.loads(self.raw_value)
         return self.value
